418c34aa404e236adc0772e56eb0c63ace7266ea
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2022-04-07 09:41:14 +0200

    Add more parameters to autoencoder data and training scripts and tweak them a little.

--- src/scrape_gym_images.py
+++ src/scrape_gym_images.py
@@ -10,6 +10,7 @@ parser.add_argument("output_path", type=str)
 parser.add_argument("--gym-env", type=str, default="MsPacman-v4")
 parser.add_argument("--rollouts", type=int, default=1024)
 parser.add_argument("--probability", type=float, default=0.01)
+parser.add_argument("--test-split", type=float, default=0.1)
 args = parser.parse_args()
 
 # Import these after checking for argparse errors to save time.
@@ -34,10 +35,16 @@ for i in range(args.rollouts):
         if done:
             break
 
+random.shuffle(observations)
+num_test = int(len(observations) * args.test_split)
+test_data = np.array(observations[:num_test], dtype=np.float32)
+train_data = np.array(observations[num_test:], dtype=np.float32)
+test_dataset = tf.data.Dataset.from_tensor_slices(test_data)
+train_dataset = tf.data.Dataset.from_tensor_slices(train_data)
+
 print(f"Observation shape: {observations[0].shape}")
 print(f"Total number of observations: {len(observations)}")
-print(f"... saving dataset to: {args.output_path}")
-
-observations = np.array(observations, dtype=np.float32)
-dataset = tf.data.Dataset.from_tensor_slices(observations)
-tf.data.experimental.save(dataset, args.output_path)
+print(f"... saving training dataset ({len(train_data)}) to: {args.output_path}_train")
+tf.data.experimental.save(train_dataset, f"{args.output_path}_train")
+print(f"... saving test dataset ({len(test_data)}) to: {args.output_path}_test")
+tf.data.experimental.save(test_dataset, f"{args.output_path}_test")
--- src/train_autoencoder.py
+++ src/train_autoencoder.py
@@ -1,4 +1,5 @@
 import argparse
+import pickle
 
 desc = """
 Trains an autoencoder on a given dataset and outputs some comparisons of the
@@ -6,8 +7,12 @@ raw dataset to the autoencoder's reconstructions of the dataset.
 """
 
 parser = argparse.ArgumentParser(description=desc)
-parser.add_argument("input_dataset", type=str)
+parser.add_argument("train_dataset", type=str)
+parser.add_argument("test_dataset", type=str)
 parser.add_argument("output_dir", type=str)
+parser.add_argument("--epochs", type=int, default=10)
+parser.add_argument("--batch-size", type=int, default=64)
+parser.add_argument("--latent-dim", type=int, default=1024)
 parser.add_argument("--normalise-dataset", default=False, action="store_true")
 parser.add_argument("--debug", default=False, action="store_true")
 args = parser.parse_args()
@@ -18,34 +23,45 @@ from tensorflow.keras import losses
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-# Debug information for tensorflow:
+# Debug information to ensure we're actually using a GPU:
+gpus = tf.config.list_physical_devices("GPU")
+print(f"GPU devices recognised by tensorflow: {gpus}")
 if args.debug:
     tf.debugging.set_log_device_placement(True)
 
-    gpus = tf.config.list_physical_devices("GPU")
-    print(f"GPU devices recognised by tensorflow: {gpus}")
+# Load and potentially normalise the datasets.
+def preprocess(dataset_path):
+    dataset = tf.data.experimental.load(dataset_path)
+    dataset = dataset.shuffle(1024) # buffer size for shuffling
+    if args.normalise_dataset:
+        dataset = dataset.map(lambda x: x / 255.0) # rescale to [0, 1]
+    
+    batched_dataset = dataset.map(lambda x: (x, x)) # input is output
+    batched_dataset = batched_dataset.batch(args.batch_size)
+    return dataset, batched_dataset
 
-# Load and potentially normalise the dataset.
-dataset = tf.data.experimental.load(args.input_dataset)
-dataset = dataset.shuffle(1024) # buffer size for shuffling
-if args.normalise_dataset:
-    dataset = dataset.map(lambda x: x / 255.0) # rescale to [0, 1]
-
-batched_dataset = dataset.map(lambda x: (x, x)) # input is output
-batched_dataset = batched_dataset.batch(32)
-dataset_shape = dataset.element_spec.shape
+train_ds, train_b_ds = preprocess(args.train_dataset)
+test_ds, test_b_ds = preprocess(args.test_dataset)
+dataset_shape = train_ds.element_spec.shape
 
 # Compile the autoencoder model.
-autoencoder = AutoEncoder(dataset_shape, [32, 16], 1024)
+autoencoder = AutoEncoder(dataset_shape, [32, 16], args.latent_dim)
 autoencoder.compile(optimizer='adam', loss=losses.MeanSquaredError())
 autoencoder.build([None] + dataset_shape)
 
 print("Summary of the compiled model:")
 print(autoencoder.summary(), "\n")
 
-print("Starting the training process...")
+# Train the model.
+print("Training the model:")
 history = autoencoder.fit(
-    batched_dataset,
-    epochs=1000,
-    validation_data=batched_dataset # TODO: separate training set
+    train_b_ds,
+    epochs=args.epochs,
+    validation_data=test_b_ds
 )
+
+# Write artifacts to disk.
+print("Writing artifacts to disk.")
+autoencoder.save_weights(f"{args.output_dir}/autoencoder.tf")
+with open(f"{args.output_dir}/history.pkl", "wb") as writer:
+    writer.write(pickle.dumps(history.history))
